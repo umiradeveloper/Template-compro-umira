c0b65f9739827dfe5d8ea1e312a1514253fb3e7b
umiradeveloper <umiradeveloper@gmail.com>
2026-03-09 09:39:08 +0700

    about translate

--- package/src/app/[locale]/(site)/about/page.tsx
+++ package/src/app/[locale]/(site)/about/page.tsx
@@ -1,21 +1,23 @@
+"use client";
 import AboutusDetail from "@/app/[locale]/components/about/aboutus-detail";
 import AboutusFullimg from "@/app/[locale]/components/about/aboutus-fullimg";
 import AboutusStats from "@/app/[locale]/components/about/aboutus-stats";
 //import Team from "@/app/components/home/team";
 import Herobanner from "@/app/[locale]/components/shared/hero-banner";
 import { Metadata } from "next";
-export const metadata: Metadata = {
-    title: "About Us | Umira Sinergi Global",
-};
+import { useTranslations } from "next-intl";
+// export const metadata: Metadata = {
+//     title: "About Us | Umira Sinergi Global",
+// };
 
 export default function Page() {
+    const t = useTranslations("AboutHero");
     return (
         <main>
             <Herobanner
                 bannerimage="/images/about-us/banner/teamphoto.png"
-                heading="About Us"
-                desc="A rapidly growing company since 2016, providing design, engineering, procurement, and construction
-                services across Indonesia." />
+                heading= {t("heading")}
+                desc={t("desc")}/>
             <AboutusDetail />
             <AboutusStats />
             <AboutusFullimg />
