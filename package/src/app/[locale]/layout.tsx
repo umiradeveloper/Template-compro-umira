8c7a6b5f87365eeef577926e8b2a95d29bb8b138
hafid hanafi <hafidhanafid@gmail.com>
2026-03-11 11:07:51 +0700

    test

--- package/src/app/[locale]/layout.tsx
+++ package/src/app/[locale]/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
   params
 }: {
   children: React.ReactNode
- params: { locale: string }
+ params: Promise<{ locale: string }>
 }) {
    const { locale } = await params
 
